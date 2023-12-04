3b97b986dbc6f7514110029954b65f8fb1f3c133
staifa <frantisek.stainer@flexiana.com>
2023-12-04 18:25:02 +0100

    add whitespace handling

---
+++ fnl/plugins/whitespace-trim.fnl
@@ -0,0 +1,3 @@
+[{1 :cappyzawa/trim.nvim
+  :opts {:trim_first_line false
+         :ft_blocklist [:TelescopePrompt :Trouble :help]}}]
---
+++ lua/plugins/whitespace-trim.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/whitespace-trim.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"cappyzawa/trim.nvim", opts = {ft_blocklist = {"TelescopePrompt", "Trouble", "help"}, trim_first_line = false}}}
